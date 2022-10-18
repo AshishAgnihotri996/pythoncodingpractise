c63c724e9cb0a3b87a82c0f12985eed04a547697
AshishAgnihotri996 <ashishagnihotri996@gmail.com>
2022-10-18 23:43:23 +0530

    "added leetode practise q3"

--- leet_code practise.py
+++ leet_code practise.py
@@ -24,3 +24,45 @@
 # s = Solution()
 # ans = s.sumequalsk(arr)
 # print(ans)
+
+#Atoi
+#
+# class Solution:
+#     def Atoi(self,strlst):
+#
+#         strlst = str.strip(' ')
+#         if not strlst:
+#             return 0
+#
+#         out = 0
+#         global negative
+#         negative = False
+#
+#         if strlst[0] == '-':
+#             negative = True
+#         elif strlst[0] == '+':
+#             negative = False
+#
+#         elif not strlst[0].isnumeric:
+#             return 0
+#         else:
+#             out = ord(strlst[0]) - ord(strlst[0])
+#
+#         for i in range(1,len(strlst)):
+#             if strlst[i].isnumeric():
+#                 out = out*10 +(ord(strlst[i])-ord("0"))
+#                 if not negative and out>= 2147483648:
+#                                return 2147483648
+#                 if negative and out >= 2147483648:
+#                     return -2147483648
+#
+#                 else:
+#                     break
+#         if not negative:
+#             return out
+#
+# s = Solution()
+# ans= s.Atoi('1231')
+# print(ans)
+
+
