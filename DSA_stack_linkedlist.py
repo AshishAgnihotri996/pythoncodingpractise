831ba54c9a1a8a955878f51df533de7db5677a21
AshishAgnihotri996 <ashishagnihotri996@gmail.com>
2022-12-08 22:29:06 +0530

    "# stack using linked list"

---
+++ DSA_stack_linkedlist.py
@@ -0,0 +1,10 @@
+class Node:
+    def __int__(self,value):
+        self.value = value
+        self.next = next
+
+class Linkedlist:
+    def __int__(self):
+        self.head = None
+
+class Stack:
